6eed91065183ac913b811d6d0d9a49afb09b335a
Joseph Henry <josephjah@gmail.com>
2019-05-03 16:09:25 -0700

    Added devel, head, and stable sections to formula

--- libzt.rb
+++ libzt.rb
@@ -1,18 +1,38 @@
 class Libzt < Formula
   desc "ZeroTier (libzt): An encrypted P2P networking layer for applications"
   homepage "https://www.zerotier.com"
-  head "https://github.com/zerotier/libzt.git", :branch => "experimental"
-
-  revision 1
-  version "1.2.0"
+  
   depends_on "make" => :build
   depends_on "cmake" => :build
+
+  stable do
+    version "1.3.0"
+    url "https://github.com/zerotier/libzt.git", :branch => "master", :revision => "2a377146d6124bb004b9aa263c47f7df2366e7ea"
+  end
+
+  head do
+    url "https://github.com/zerotier/libzt.git"
+  end
+
+  devel do
+    version "1.3.1"
+    url "https://github.com/zerotier/libzt.git", :branch => "dev"
+  end
 
   def install
     system "make", "update"
     system "cmake", ".", *std_cmake_args
     system "cmake", "--build", "."
     system "make", "install"
+    system "cp", "LICENSE.GPL-3", "#{prefix}/LICENSE"
+  end
+
+  def caveats
+    <<~EOS
+      Visit https://my.zerotier.com to create virtual networks and authorize devices.
+      Visit https://www.zerotier.com/manual.shtml to learn more about how ZeroTier works.
+      Visit https://github.com/zerotier/ZeroTierOne/tree/master/controller to learn how to run your own network controller (advanced).
+    EOS
   end
 
   test do
