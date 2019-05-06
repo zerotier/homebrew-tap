b5405758ae92990d1514e97ab3f55cd60ef3f5f0
Joseph Henry <josephjah@gmail.com>
2019-05-06 11:42:17 -0700

    Added bottle section and removed test section

--- libzt.rb
+++ libzt.rb
@@ -5,6 +5,12 @@ class Libzt < Formula
   depends_on "make" => :build
   depends_on "cmake" => :build
 
+  bottle do
+    root_url "https://download.zerotier.com/homebrew/libzt--1.3.0.mojave.bottle.tar.gz"
+    cellar :any
+    sha256 "e1ac8425fd0ea510c7db734af8d6c41cd3650b12f66a571f9d818c0121422eee" => :mojave
+  end
+
   stable do
     version "1.3.0"
     url "https://github.com/zerotier/libzt.git", :branch => "master", :revision => "3d1159882117278fcb5fabb623bd62175b6c7e6c"
@@ -24,7 +30,7 @@ class Libzt < Formula
     system "cmake", ".", *std_cmake_args
     system "cmake", "--build", "."
     system "make", "install"
-    system "cp", "LICENSE.GPL-3", "#{prefix}/LICENSE"
+    system "cp", "LICENSE*", "#{prefix}/LICENSE"
   end
 
   def caveats
@@ -33,9 +39,5 @@ class Libzt < Formula
       Visit https://www.zerotier.com/manual.shtml to learn more about how ZeroTier works.
       Visit https://github.com/zerotier/ZeroTierOne/tree/master/controller to learn how to run your own network controller (advanced).
     EOS
-  end
-
-  test do
-    system "false"
   end
 end
