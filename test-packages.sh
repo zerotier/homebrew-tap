9fb98cf950f72b26df4d794344c3b8a026d7e26a
Erik Hollensbe <linux@hollensbe.org>
2021-05-13 13:48:39 -0700

    zeronsd formula
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- test-packages.sh
+++ test-packages.sh
@@ -7,6 +7,12 @@ packages=(
 
 cmdline="brew tap zerotier/test-tap file:///taps && (for pkg in ${packages[@]}; do brew install zerotier/test-tap/\$pkg; done)"
 
+if [ "$(git status -s | grep -v \? | wc -l)" != 0 ]
+then
+  echo "You have a dirty tree; please commit it before continuing"
+  exit 1
+fi
+
 case "$(uname -s)" in
   Linux)
     docker run -it -v ${PWD}:/taps linuxbrew/brew bash -c "$cmdline"
