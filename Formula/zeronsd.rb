35cccca4df16fbc4b2a769bbb10553d5af9fec11
Erik Hollensbe <linux@hollensbe.org>
2021-06-23 11:30:54 -0700

    zeronsd 0.2.1 (0.2.0 was skipped due to compilation issues)
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- Formula/zeronsd.rb
+++ Formula/zeronsd.rb
@@ -1,12 +1,12 @@
 class Zeronsd < Formula
-  @@version = "0.1.7"
+  @@version = "0.2.1"
 
   desc "Name service for ZeroTier Central"
   version @@version
   homepage "https://www.zerotier.com"
   url "https://github.com/zerotier/zeronsd.git",
     tag:      "v#{@@version}",
-    revision: "d65f1a71026807ee02f2628ea6a7d2a4b50f3b3a"
+    revision: "22ec4d63aa61c9c4b938d7cd8b8813c63d8628a7"
   license "BSD-3-Clause"
 
   depends_on "pkg-config" => :build
