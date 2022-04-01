6c43fafbb85221b3e8cb119d9a2d209f854373a0
Erik Hollensbe <git@hollensbe.org>
2022-03-31 18:52:05 -0700

    zeronsd 0.4.1
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- Formula/zeronsd.rb
+++ Formula/zeronsd.rb
@@ -1,12 +1,12 @@
 class Zeronsd < Formula
-  @@version = "0.4.0"
+  @@version = "0.4.1"
 
   desc "Name service for ZeroTier Central"
   version @@version
   homepage "https://www.zerotier.com"
   url "https://github.com/zerotier/zeronsd.git",
     tag:      "v#{@@version}",
-    revision: "8d66068caf10d5471719203577610704f116577a"
+    revision: "7543691f4c4fc24f59c58c9e460ecf0821f36759"
   license "BSD-3-Clause"
 
   depends_on "pkg-config" => :build
